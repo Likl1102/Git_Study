b72749c41e6866135a71e189851821ab8688df8f
likl1102 <13036784+likl1102@user.noreply.gitee.com>
2023-09-08 14:33:38 +0800

    v1

---
+++ get_url.py
@@ -0,0 +1,7 @@
+import requests
+
+url = "http://127.0.0.1:8000/business1coverage/"
+# params = {'key': 'value'}
+# headers = {'Content-Type': 'application/json'}
+
+response = requests.get(url=url)
